bc0f912badfc1f45bbc7d02d9e218fbab103ab96
Eduard Kudlanov <eduardokud@mail.ru>
2022-07-28 21:25:44 +0300

    100% test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxNofirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
 }
